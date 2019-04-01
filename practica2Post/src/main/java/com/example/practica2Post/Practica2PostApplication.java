37c0d2d8411b36db5b76894a74d50a360740fdd3
pedroARV <pedro.antonio.reyes.valdes@gmail.com>
2019-04-01 16:47:33 -0600

    primer commit practica 2

---
+++ practica2Post/src/main/java/com/example/practica2Post/Practica2PostApplication.java
@@ -0,0 +1,13 @@
+package com.example.practica2Post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2PostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2PostApplication.class, args);
+	}
+
+}
