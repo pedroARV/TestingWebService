25a7c90ace137db052d4b1c668ff63d69ef58438
pedroARV <pedro.antonio.reyes.valdes@gmail.com>
2019-04-03 10:38:21 -0600

    eliminando codigo sobrante

--- practica2Post/src/main/java/com/example/practica2Post/controller/UsuarioController.java
+++ practica2Post/src/main/java/com/example/practica2Post/controller/UsuarioController.java
@@ -1,7 +1,6 @@
 package com.example.practica2Post.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,26 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.practica2Post.model.Usuarios;
 import com.example.practica2Post.service.IUserService;
-import com.example.practica2Post.business.UserBusiness;
 import com.example.practica2Post.model.Response;
 
 @RestController
 public class UsuarioController{
 	
 	@Autowired IUserService userService;
-	
-	@Autowired UserBusiness userBusiness;
-	
+		
 	@PostMapping("/users")
 	public ResponseEntity<Response> respuesta(@RequestHeader (value="fileName",required=true )String fileName,@RequestBody Usuarios usuarios) throws Throwable{	
-		
-		ResponseEntity<Response> response = new ResponseEntity<Response>(HttpStatus.OK);
-		
-		
-		response = userService.nombreArchivo(fileName);
-
-		return response;
-	}
+		return userService.nombreArchivo(fileName);
+		}
 	
 
 }
